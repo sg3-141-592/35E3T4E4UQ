5fe37fef989f64464dca7db6b1a10ab4c236c2c7
sean <GilliganS@ipslazure.onmicrosoft.com>
2022-10-21 06:58:18 +0100

    bank

--- root_redirect_app/app/management/commands/check_a_records.py
+++ root_redirect_app/app/management/commands/check_a_records.py
@@ -5,5 +5,10 @@ from app.models import Site
 class Command(BaseCommand):
     help = 'check the a record mapping for site'
 
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('site_id', nargs='+', type=int)
+        return super().add_arguments(parser)
+
     def handle(self, *args, **options):
+        print(options['site_id'])
         self.stdout.write("ABC {}".format(Site.objects.count()))
